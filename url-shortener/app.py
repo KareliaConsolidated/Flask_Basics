4f09fb2310d8a4431bbc0dc6986e08c26134ec1c
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-02-27 18:16:53 +0530

    URL-Shortener: JSON Transfer:

--- url-shortener/app.py
+++ url-shortener/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -17,6 +18,10 @@ def about():
 
 def your_url():
 	if request.method == 'POST':
+		urls = {}
+		urls[request.form['code']] = {'url': request.form['url']}
+		with open('urls.json','w') as url_file:
+			json.dump(urls, url_file)
 		return render_template('your_url.html', code = request.form['code'])
 	else:
-		return 'This is not Valid !'		
+		return redirect(url_for('home'))
